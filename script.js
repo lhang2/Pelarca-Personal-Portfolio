32e8d57376ea1171ce30c5ea2c48632cc7839477
lhang2 <97422308+lhang2@users.noreply.github.com>
2022-10-13 20:56:02 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Blogger", "UI Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Developer", "Blogger", "UI Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
